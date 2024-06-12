2cd452e7d404989a54254697a2dc2a834d26589f
Manifesto <163677599+TheAnarchoCriteria@users.noreply.github.com>
2024-06-12 20:57:58 +0200

    Add files via upload

--- localizations/resources_es.js
+++ localizations/resources_es.js
@@ -1,7 +1,7 @@
 const resources_es = {
   pageTitle: "Manifesto",
   logoAlt: "Logo de la marca",
-  productName: "Falda con secretos",
+  productName: "The Uncensored Suit Jacket",
   imageAlt: "Foto del artículo",
   menuItems: [
     "Código del producto",
